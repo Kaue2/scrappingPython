c4bacac9be4421156d9d1c29b52782b9cded9cc2
Kauê Anjos <kaueo.anjos@gmail.com>
2024-11-27 17:29:39 -0300

    feat(product e main): função para salvar todos os produtos de uma página

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-import htmlPage
 import seleniumHtml
 from bs4 import BeautifulSoup
-import re
+from product import Product
 
 url = "https://www.terabyteshop.com.br/"
-page = htmlPage.requests(url)
-page2 = seleniumHtml.htmlpage(url)
+page = seleniumHtml.htmlpage(url)
+prods = []
 
-
-doc = BeautifulSoup(page2.htmlContent, 'html.parser')
+doc = BeautifulSoup(page.htmlContent, 'html.parser')
 products_contents = doc.findAll("div", {"class":"product-item__content"})
-product_info = products_contents[0].findAll(name=["div", "a"], class_=lambda c : c in ["product-item__name", "product-item__new-price"])
-content = product_info[0].getText()
 
-print(content)
 
-for i in range (len(product_info)):
-    print("%s \n" % (product_info[i]))
+# try catch
+for i in range(len(products_contents)):
+    product_info = products_contents[i].findAll(name=["div", "a"], class_=lambda c : c in ["product-item__name", "product-item__new-price"])
+    #print("%s \n" % (product_info[i]))
+    #print(product_info[0].getText())
+    #print(product_info[1].getText())
+    prod = Product(product_info[0].getText(), product_info[1].getText())
+    prods.append(prod)
+
+
+for i in range(len(prods)):
+    print("Name: %s" % (prods[i].name))
+    print("Preço: %f" % (prods[i].price))
+
---
+++ product.py
@@ -0,0 +1,11 @@
+import re
+
+class Product:
+    def __init__(self, name, price) -> None:
+        self.name = name
+        self.price = ''
+        self.treatPrice(price)
+
+    def treatPrice(self, price) -> None:
+        price = re.sub(r'[^\d,]', '', price)
+        self.price = float(re.sub(r',', '.', price))
